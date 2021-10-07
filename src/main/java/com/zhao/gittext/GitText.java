bdecd29586e50b831562ec237d0729a28a670098
xiaoZhao <zpzgzsby@163.com>
2021-10-07 11:25:26 +0800

    first commit

---
+++ src/main/java/com/zhao/gittext/GitText.java
@@ -0,0 +1,13 @@
+package com.zhao.gittext;
+
+/**
+ * @author xiaoZhao
+ * @date 2021/10/7
+ * @describe
+ */
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("Hello git !");
+        System.out.println("sss");
+    }
+}
---
+++ src/main/java/com/zhao/gittext/Text.java
@@ -0,0 +1,14 @@
+package com.zhao.gittext;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author xiaoZhao
+ * @date 2021/10/7
+ * @describe
+ */
+public class Text {
+    public static void main(String[] args) {
+        System.out.println("222");
+    }
+}
