8cdaeaf05a4432269c08e05a5cc57fb1cdee0686
xiaoZhao <zpzgzsby@163.com>
2021-10-07 11:28:13 +0800

    third commit

--- src/main/java/com/zhao/gittext/Text.java
+++ src/main/java/com/zhao/gittext/Text.java
@@ -11,5 +11,6 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("222");
         System.out.println("2225");
+        System.out.println("2225");
     }
 }
